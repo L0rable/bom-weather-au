7df9da7e34bdf4bd50c4a5de77f79514711c64ed
L0rable <finwalker@gmail.com>
2025-07-29 21:45:01 +1000

    Connect to FTP server and retrieve BOM data

--- main.go
+++ main.go
@@ -1,7 +1,48 @@
 package main
 
-import ("log")
+import (
+	"io"
+	"log"
+	"time"
+
+	"github.com/jlaffaye/ftp"
+)
+
+const FTP_URL = "ftp.bom.gov.au:21"
+
+func openFtpServer() *ftp.ServerConn {
+	conn, err := ftp.Dial(FTP_URL, ftp.DialWithTimeout(5*time.Second))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = conn.Login("anonymous", "")
+	if err != nil {
+		log.Fatal(err)
+	}
+	return conn
+}
+
+func closeFtpServer(conn *ftp.ServerConn) {
+	err := conn.Quit()
+	if err != nil {
+		log.Fatal(err)
+	}
+}
 
 func main() {
-	log.Println("Hello BOM!")
+	conn := openFtpServer()
+	obsURL := "/anon/gen/fwo/IDV60920.xml"
+	resp, err := conn.Retr(obsURL)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	data, err := io.ReadAll(resp)
+	if err != nil {
+		log.Println(data)
+	}
+	log.Println(string(data))
+
+	closeFtpServer(conn)
 }
